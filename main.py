d3ee73f064932605aae35447bc1bb31e97e3563a
HerbertNtim <herbertntim15@gmail.com>
2026-07-18 04:33:12 +0000

    path parameters

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def index() :
   return "Hello World!!!!"
+
+@app.get('/blog/all')
+def get_all_blog():
+  return "Getting all blogs"
+
+@app.get("/blog/{id}")
+def get_blog(id: int):
+  return {"message": f"Blog with the id {id}"}
+
