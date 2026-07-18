eed99bdaed64e3e8a3c5d936e9c9db966d10d50c
HerbertNtim <herbertntim15@gmail.com>
2026-07-18 04:45:30 +0000

    predefined types

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -9,6 +10,15 @@ def index() :
 @app.get('/blog/all')
 def get_all_blog():
   return "Getting all blogs"
+
+class BlogType(str, Enum):
+  short = 'short'
+  story = 'story'
+  howto = 'howto'
+  
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+  return {'message': f'This is a Blog type of {type}'}
 
 @app.get("/blog/{id}")
 def get_blog(id: int):
