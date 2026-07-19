ff3e614c7aa36c84ae3353f2b317ba1504334d02
HerbertNtim <herbertntim15@gmail.com>
2026-07-19 09:17:56 +0000

    operation description

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 
 app = FastAPI()
@@ -24,7 +24,12 @@ class BlogType(str, Enum):
 def get_blog_type(type: BlogType):
   return {'message': f'This is a Blog type of {type}'}
 
-@app.get("/blog/{id}")
-def get_blog(id: int):
-  return {"message": f"Blog with the id {id}"}
+@app.get("/blog/{id}",  status_code=status.HTTP_200_OK)
+def get_blog(id: int, response:Response):
+  if id > 5:
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'error': f"Blog with {id} not found"}
+  else:    
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"Blog with the id {id} found"}
 
