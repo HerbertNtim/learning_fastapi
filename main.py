45008316e603fb5258ecb9c4545780dd5adeea51
HerbertNtim <herbertntim15@gmail.com>
2026-07-19 09:37:25 +0000

    tags

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, status, Response
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -11,20 +12,24 @@ def index() :
 # def get_all_blog():
 #   return "Getting all blogs"
 
-@app.get('/blog/all')
+@app.get('/blog/all', tags=['blog'])
 def get_blogs(page=1, page_size=10):
   return {"message": f'All {page_size} blogs can be found on {page}'}
+
+@app.get('/blog/comments/{comment_id}', tags=['blog', 'comment'])
+def get_comments(comment_id: int, valid: bool = True, username: Optional[str] = None):
+  return {"message": f"Blog ID {comment_id}, valid {valid}, username {username}"}
 
 class BlogType(str, Enum):
   short = 'short'
   story = 'story'
   howto = 'howto'
   
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
   return {'message': f'This is a Blog type of {type}'}
 
-@app.get("/blog/{id}",  status_code=status.HTTP_200_OK)
+@app.get("/blog/{id}",  status_code=status.HTTP_200_OK, tags=['blog'])
 def get_blog(id: int, response:Response):
   if id > 5:
     response.status_code = status.HTTP_404_NOT_FOUND
